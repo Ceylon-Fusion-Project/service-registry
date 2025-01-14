499c00ea9dfa26d359d89d414f316f5678be88dd
Harsha Udara <hudaragc@gmail.com>
2025-01-14 14:00:06 +0530

    Initial Commit

---
+++ src/test/java/com/ceylon_fusion/service_registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ceylon_fusion.service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
